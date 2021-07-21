e3e4b41a590f841064916dedadb54b58610e6fd4
Elkhan <ismayilzadeelxan@gmail.com>
2021-07-21 18:39:50 +0900

    Update tscontext.py

--- my_imp/tscontext.py
+++ my_imp/tscontext.py
@@ -4,41 +4,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import beta
 import ast,sys,random
-
-NUM_TRIALS = 20000
+import math
+NUM_TRIALS = 2000
 BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
 
 
 class Bandit:
-  def __init__(self, contextDimension, v_squared, trueMean, context):
+  def __init__(self, contextDimension):
       self.d = contextDimension
       self.B = np.identity(self.d)
-      self.v_squared = v_squared
+      v = 0.001 * math.sqrt(24 * self.d / 0.05 * math.log(1 / 0.1))
+      self.v_squared = v**2
       self.f = np.zeros(self.d)
-      self.trueMean = np.dot(np.transpose(context),trueMean)
+      self.trueMean = np.random.random((self.d,1))
       self.theta_hat = np.zeros(self.d)
       self.theta_estimate = np.random.multivariate_normal(self.theta_hat, self.v_squared * np.linalg.inv(self.B))
-      self.regret = abs(np.dot(np.transpose(self.theta_estimate),context)-self.trueMean)
+      # self.regret = abs(np.dot(np.transpose(self.theta_estimate),context)-self.trueMean)
+      self.regret=0
       self.N = 0
-      self.myContext = context
 
-  def pull(self):
-    if self.regret <= abs(np.dot(np.transpose(self.myContext),self.theta_estimate)-self.trueMean):
-        return 0
-    else:
-        self.regret = abs(np.dot(np.transpose(self.myContext),self.theta_estimate)-self.trueMean)
-        return 1
+  def pull(self,context):
+      return context.dot(self.trueMean)
+
   def sample(self):
     self.theta_estimate = np.random.multivariate_normal(self.theta_hat, self.v_squared * np.linalg.inv(self.B))
-    return np.dot(np.transpose(self.myContext),self.theta_estimate)
+    return self.theta_estimate
 
-  def update(self, reward):
-    self.B += np.outer(self.myContext,self.myContext)
-    self.f += self.myContext*reward
+  def update(self, reward,context):
+    self.B += np.outer(context,context)
+    self.f += context*reward
     self.theta_hat = np.dot(np.linalg.inv(self.B),self.f)
     self.N += 1
   def getEstimate(self):
-      return np.dot(np.transpose(self.myContext),self.theta_estimate)
+      return self.theta_estimate
 def plot(bandits, trial):
   x = np.linspace(0, 1, 200)
   for b in bandits:
@@ -50,31 +48,27 @@ def plot(bandits, trial):
 
 
 def experiment():
-  BANDIT_PROBABILITIES = ast.literal_eval(sys.argv[1])
-  BANDIT_CONTEXTS = []
+  BANDIT_PROBABILITIES = 10
   np.random.seed(42)
-  for i in range(len(BANDIT_PROBABILITIES)):
-      sample = np.ones(10)
-      BANDIT_CONTEXTS.append(sample)
-  bandits = [Bandit(10,25,np.ones(10)*p,BANDIT_CONTEXTS[k]) for k,p in enumerate(BANDIT_PROBABILITIES)]
+  bandits = [Bandit(10) for k in range(BANDIT_PROBABILITIES)]
   sample_points = [5,10,20,50,100,200,500,1000,1500,1999]
   rewards = np.zeros(NUM_TRIALS)
   for i in range(NUM_TRIALS):
     # Thompson sampling
-    j = np.argmax([b.sample() for b in bandits])
-
+    context = np.hstack([np.random.random((10,1)) for p in range(BANDIT_PROBABILITIES)])
+    j = np.argmax([np.dot(context[:,t].T,b.sample()) for t,b in enumerate(bandits)])
     # plot the posteriors
     # if i in sample_points:
     #   plot(bandits, i)
 
     # pull the arm for the bandit with the largest sample
-    x = bandits[j].pull()
+    x = bandits[j].pull(context[:,j])
 
     # update rewards
     rewards[i] = x
 
     # update the distribution for the bandit whose arm we just pulled
-    bandits[j].update(x)
+    bandits[j].update(x,context[:,j])
 
   # print total reward
   print("total reward earned:", rewards.sum())
