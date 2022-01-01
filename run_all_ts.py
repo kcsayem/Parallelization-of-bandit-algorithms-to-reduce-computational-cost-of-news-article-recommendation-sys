fa0498994b051c5714acbc101c167761e1c15fa9
elkhanzada <ismayilzadeelxan@gmail.com>
2022-01-01 23:48:57 +0900

    cleanup code

--- run_all_ts.py
+++ run_all_ts.py
@@ -1,4 +1,4 @@
-from thompson import LazyThompsonSampling, NonLazyThompsonSampling, ThompsonSampling
+from thompson import LazyThompsonSampling, NonLazyThompsonSampling
 from helper_functions import *
 from helper_functions import print_example_banner as ptb
 from tqdm import tqdm
@@ -9,7 +9,6 @@ from datetime import datetime
 import logging
 from run_linucb import lazy_experiment as linLazy
 from run_linucb import non_lazy_experiment as linNonLazy
-from run_linucb import experiment as linSeq
 import time
 
 SEED = 42
@@ -23,13 +22,11 @@ def run_seq_par(processes, filename_ts, filename_ln):
     path = "data/R6A_spec"
     ptb("RUNNING LINUCB EXPERIMENTS")
     ptb("RUNNING SEQUENTIAL EXPERIMENTS")
-    print("RUNNING SEQUENTIAL EXPERIMENTS")
     start = datetime.now()
     linNonLazy(path, [processes[0]], filename_ln)
     end = datetime.now()
     ptb(f"DURATION FOR P = {processes[0]}: {end - start}")
     ptb("RUNNING NONLAZY EXPERIMENTS")
-    print("RUNNING NONLAZY EXPERIMENTS")
     start = datetime.now()
     linNonLazy(path, [processes[1]], filename_ln)
     end = datetime.now()
@@ -40,15 +37,12 @@ def run_seq_par(processes, filename_ts, filename_ln):
 
     ptb("RUNNING THOMPSON EXPERIMENTS")
     ptb("RUNNING SEQUENTIAL EXPERIMENTS")
-    print("RUNNING THOMPSON EXPERIMENTS")
-    print("RUNNING SEQUENTIAL EXPERIMENTS")
     start = datetime.now()
     experiment(path, processes[0], False, filename_ts)
     end = datetime.now()
     ptb(f"DURATION FOR P = {processes[0]}: {end - start}")
 
     ptb("RUNNING NONLAZY EXPERIMENTS")
-    print("RUNNING NONLAZY EXPERIMENTS")
     start = datetime.now()
     experiment(path, processes[1], False, filename_ts)
     end = datetime.now()
@@ -68,7 +62,6 @@ def run_par_par(processes, filename_ts, filename_ln):
     end = datetime.now()
     ptb(f"DURATION FOR P = {processes[0]}: {end - start}")
     ptb("RUNNING NONLAZY EXPERIMENTS")
-    print("RUNNING NONLAZY EXPERIMENTS")
     start = datetime.now()
     linNonLazy(path, [processes[0]], filename_ln)
     end = datetime.now()
@@ -79,27 +72,22 @@ def run_par_par(processes, filename_ts, filename_ln):
 
     ptb("RUNNING THOMPSON EXPERIMENTS")
     ptb("RUNNING SEQUENTIAL EXPERIMENTS")
-    print("RUNNING THOMPSON EXPERIMENTS")
-    print("RUNNING LAZY EXPERIMENTS")
     start = datetime.now()
     experiment(path, processes[0], True, filename_ts)
     end = datetime.now()
     ptb(f"DURATION FOR P = {processes[0]}: {end - start}")
     ptb("RUNNING NONLAZY EXPERIMENTS")
-    print("RUNNING NONLAZY EXPERIMENTS")
     start = datetime.now()
     experiment(path, processes[0], False, filename_ts)
     end = datetime.now()
     ptb(f"DURATION FOR P = {processes[0]}: {end - start}")
 
 
-def parallel_experiment(path, v, articles, ts, aligned_time_steps, cumulative_rewards, aligned_ctr,
+def parallel_experiment(path, articles, ts, aligned_time_steps, cumulative_rewards, aligned_ctr,
                         random_aligned_time_steps, random_cumulative_rewards, random_aligned_ctr, t, p):
     f = open(path, "r")
     max_ = get_num_lines(path)
     for iteration in tqdm(range(math.ceil(max_ / p))):
-        # if iteration >= 100:
-        #     break
         lines = []
         for i in range(p):
             lines.append(f.readline())
@@ -121,63 +109,14 @@ def parallel_experiment(path, v, articles, ts, aligned_time_steps, cumulative_re
             if type(ts) is LazyThompsonSampling:
                 ts.update_iteration(contexts[c][arm_index])
         for r in range(len(contexts)):
-            # random_index = np.random.choice(specific_bandits).index
             if arm_indexes[r] == article_ids[r]:
-                # Use reward information for the chosen arm to update
-
-                # For CTR calculation
                 aligned_time_steps += 1
                 cumulative_rewards += clicks[r]
                 aligned_ctr.append(cumulative_rewards / aligned_time_steps)
             else:
                 clicks[r] = 0
-        # ts.doubling_round()
         ts.update_batch(clicks, contexts)
-        # if v == 0.01 and random_index == int(articleID):
-        #     random_aligned_time_steps += 1
-        #     random_cumulative_rewards += click
-        #     random_aligned_ctr.append(
-        #         random_cumulative_rewards / random_aligned_time_steps)
         t += 1
-        # if t == 2:
-        #     break
-    # ts.updateV(t)
-    f.close()
-    return ts, aligned_time_steps, cumulative_rewards, aligned_ctr, random_aligned_time_steps, random_cumulative_rewards, random_aligned_ctr, t
-
-
-def sequential_experiment(path, v, articles, ts, aligned_time_steps, cumulative_rewards, aligned_ctr,
-                          random_aligned_time_steps, random_cumulative_rewards, random_aligned_ctr, t):
-    f = open(path, "r")
-    max_ = get_num_lines(path)
-    iteration = 0
-    for line_data in tqdm(f, total=max_):
-        # iteration+=1
-        # if iteration==5000:
-        #     break
-        tim, articleID, click, user_features, pool_articles = parseLine(
-            line_data)
-        context = makeContext(pool_articles, user_features, articles)
-        # print(context)
-        # break
-        x, arm_index, specific_bandits = ts.pull(context)
-        # random_index = np.random.choice(specific_bandits).index
-        if arm_index == int(articleID):
-            # Use reward information for the chosen arm to update
-            ts.update(click, context[arm_index])
-            # For CTR calculation
-            aligned_time_steps += 1
-            cumulative_rewards += click
-            aligned_ctr.append(cumulative_rewards / aligned_time_steps)
-        # if v == 0.01 and random_index == int(articleID):
-        #     random_aligned_time_steps += 1
-        #     random_cumulative_rewards += click
-        #     random_aligned_ctr.append(
-        #         random_cumulative_rewards / random_aligned_time_steps)
-        # t += 1
-        # if t == 2:
-        #     break
-        # ts.updateV(t)
     f.close()
     return ts, aligned_time_steps, cumulative_rewards, aligned_ctr, random_aligned_time_steps, random_cumulative_rewards, random_aligned_ctr, t
 
@@ -185,12 +124,11 @@ def sequential_experiment(path, v, articles, ts, aligned_time_steps, cumulative_
 def experiment(folder, p, lazy, savefile):
     articles = get_all_articles()
     v_s = [0.3]
-    random_results = []
     for v in v_s:
         v = float("{:.2f}".format(v))
-        ts = LazyThompsonSampling(len(articles) * 6 + 6, 0.0001, v, 1, SEED) if lazy else NonLazyThompsonSampling(
-            len(articles) * 6 + 6, 0.0001, v, 1, SEED)
-        ts.setUpBandits(articles)
+        ts = LazyThompsonSampling(len(articles) * 6 + 6, v, 1, SEED) if lazy else NonLazyThompsonSampling(
+            len(articles) * 6 + 6, v, 1, SEED)
+        ts.setup_bandits(articles)
         aligned_time_steps = 0
         random_aligned_time_steps = 0
         cumulative_rewards = 0
@@ -202,56 +140,17 @@ def experiment(folder, p, lazy, savefile):
             for filename in files:
                 path = os.path.join(root, filename)
                 ts, aligned_time_steps, cumulative_rewards, aligned_ctr, random_aligned_time_steps, random_cumulative_rewards, random_aligned_ctr, t = parallel_experiment(
-                    path, v, articles, ts, aligned_time_steps, cumulative_rewards, aligned_ctr,
+                    path, articles, ts, aligned_time_steps, cumulative_rewards, aligned_ctr,
                     random_aligned_time_steps, random_cumulative_rewards, random_aligned_ctr, t, p)
         lazy_nonlazy = 'Lazy' if lazy else 'NonLazy'
         label = f'P = {p}, {lazy_nonlazy}' if p != 1 else 'Sequential'
         plt.plot(aligned_ctr, label=label)
-        # if v == 0.01:
-        #     # plt.plot(random_aligned_ctr, label=f"Random CTR")
-        #     random_results = random_cumulative_rewards
         logging.info(f"Total reward earned from Thompson: {cumulative_rewards}")
-        ts.printBandits()
-    # logging.info(f"Total reward earned from Random: {random_results}")
+        ts.print_bandits()
     plt.ylabel("CTR ratio (Thompson Sampling)")
     plt.xlabel("Time")
     plt.legend()
     plt.savefig(f"Results/{savefile}.png")
-
-
-def experiment_seq(folder):
-    plt.figure()
-    articles = get_all_articles()
-    v_s = [0.3]
-    random_results = []
-    for v in v_s:
-        v = float("{:.2f}".format(v))
-        ts = ThompsonSampling(len(articles) * 6 + 6, 0.0001, v)
-        ts.setUpBandits(articles)
-        aligned_time_steps = 0
-        random_aligned_time_steps = 0
-        cumulative_rewards = 0
-        random_cumulative_rewards = 0
-        aligned_ctr = []
-        random_aligned_ctr = []
-        t = 1
-        for root, dirs, files in os.walk(folder):
-            for filename in files:
-                path = os.path.join(root, filename)
-                ts, aligned_time_steps, cumulative_rewards, aligned_ctr, random_aligned_time_steps, random_cumulative_rewards, random_aligned_ctr, t = sequential_experiment(
-                    path, v, articles, ts, aligned_time_steps, cumulative_rewards, aligned_ctr,
-                    random_aligned_time_steps, random_cumulative_rewards, random_aligned_ctr, t)
-        plt.plot(aligned_ctr, label=f"Sequential")
-        # if v == 0.01:
-        #     plt.plot(random_aligned_ctr, label=f"Random CTR")
-        #     random_results = random_cumulative_rewards
-        logging.info(f"total reward earned from Thompson: {cumulative_rewards}")
-        ts.printBandits()
-    logging.info(f"total reward earned from Random: {random_results}")
-    plt.ylabel("CTR ratio (Thompson Sampling)")
-    plt.xlabel("Time")
-    plt.legend()
-    plt.savefig(f"Results/parallel_seq_thompson.png")
 
 
 if __name__ == "__main__":
--- thompson.py
+++ thompson.py
@@ -1,4 +1,3 @@
-import numpy as np
 from helper_functions import *
 from numpy.random import default_rng
 import logging
@@ -7,26 +6,22 @@ logger = logging.getLogger(__name__)
 
 
 class ThompsonSampling:
-    def __init__(self, contextDimension, R, c, lmb=1, SEED=42):
+    def __init__(self, d, c, lmb=1, SEED=42):
         self.rng = default_rng()
         self.SEED = SEED
-        self.d = contextDimension
+        self.d = d
         self.B = np.identity(self.d) * lmb
         self.B_inv = np.linalg.inv(self.B)
         self.B_previous = np.copy(self.B)
-        v = c  # * R * math.sqrt(24 * self.d / 0.05 * math.log(1 / 0.05))
-        self.R = R
-        self.c = c
-        self.v_squared = v ** 2
+        self.v_squared = c ** 2
         self.f = np.zeros(self.d)
         self.theta_hat = np.zeros(self.d)
         self.theta_estimate = self.rng.multivariate_normal(
             self.theta_hat, self.v_squared * self.B_inv, method='cholesky')
         self.doubling_rounds = 0
         self.bandits = []
-        self.regrets = []
 
-    def setUpBandits(self, articleIds):
+    def setup_bandits(self, articleIds):
         self.bandits = [Bandit(articleIds[k]) for k in range(len(articleIds))]
 
     def sample(self):
@@ -42,17 +37,6 @@ class ThompsonSampling:
     def update_iteration(self, context):
         self.B += np.outer(context, context)
         self.B_inv = inverse(self.B_inv, np.outer(context, context))
-
-    def update_batch(self, rewards, contexts):
-        rc_sum = 0
-        bc_sum = 0
-        for i in range(len(contexts)):
-            rc_sum += contexts[i][list(contexts[i].keys())[0]] * rewards[i]
-            bc_sum += np.outer(contexts[i][list(contexts[i].keys())[0]], contexts[i][list(contexts[i].keys())[0]])
-        self.f += rc_sum
-        self.B += bc_sum
-        self.B_inv = inverse(self.B_inv, bc_sum)
-        self.theta_hat = np.dot(self.B_inv, self.f)
 
     def doubling_round(self):
         if ispositivesemidifinate(2 * self.B_previous - self.B):
@@ -83,21 +67,11 @@ class ThompsonSampling:
         estimated_reward = max_value
         return estimated_reward, max_, specific_bandits
 
-    def getEstimate(self):
-        return self.theta_estimate
-
-    def printBandits(self):
+    def print_bandits(self):
         msg = f"Num times selected each bandit: {[b.N for b in self.bandits]}"
         logger.info(msg)
         msg = f"Doubling Rounds: {self.doubling_rounds}"
         logger.info(msg)
-
-    def getRegrets(self):
-        return self.regrets
-
-    def updateV(self, t):
-        v = self.c * self.R * math.sqrt(24 * self.d / 0.05 * math.log(t / 0.05))
-        self.v_squared = v ** 2
 
 
 class Bandit:
@@ -111,8 +85,8 @@ class Bandit:
 
 
 class LazyThompsonSampling(ThompsonSampling):
-    def __init__(self, contextDimension, R, c, lmb, SEED):
-        ThompsonSampling.__init__(self, contextDimension, R, c, lmb, SEED)
+    def __init__(self, d, c, lmb, SEED):
+        ThompsonSampling.__init__(self, d, c, lmb, SEED)
 
     def update_iteration(self, context):
         self.B += np.outer(context, context)
@@ -127,8 +101,8 @@ class LazyThompsonSampling(ThompsonSampling):
 
 
 class NonLazyThompsonSampling(ThompsonSampling):
-    def __init__(self, contextDimension, R, c, lmb, SEED):
-        ThompsonSampling.__init__(self, contextDimension, R, c, lmb, SEED)
+    def __init__(self, d, c, lmb, SEED):
+        ThompsonSampling.__init__(self, d, c, lmb, SEED)
 
     def update_batch(self, rewards, contexts):
         rc_sum = 0
